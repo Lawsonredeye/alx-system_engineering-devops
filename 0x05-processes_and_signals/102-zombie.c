4f4b6b1ad9e244b2814499ba94ad180db978f11d
lawsonredeye <omoregbeeolawson@gmail.com>
2024-01-29 00:11:32 +0100

    zombie process

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * infinite_while - main point of execution
+ *
+ * Return: zero always
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - main point of execution
+ *
+ * Return: zero always
+ */
+int main(void)
+{
+	pid_t child_pid;
+	int i;
+
+	for (i = 0; i < 5; i++)
+	{
+		child_pid = fork();
+		if (child_pid > 0)
+		{
+			printf("zombie process created, PID: %d\n", child_pid);
+		}
+		else
+			exit(0);
+	}
+	infinite_while();
+	return (0);
+}
