10f9777d4149760e682eac80d7c2d827d1292152
lawsonredeye <omoregbeeolawson@gmail.com>
2024-04-23 00:02:48 +0100

    script that prints data

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Learning about REST API and how to get data from an api service
+using either requests or urllib
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    """
+    main function which is the first poioint of execution for running this
+    script to get the userName, ID, title
+    """
+    # - Variables to be used
+    complete = 0
+    total = 0
+    args = int(sys.argv[1])
+    userUrl = requests.get("https://jsonplaceholder.typicode.com/users")
+    todoUrl = requests.get("https://jsonplaceholder.typicode.com/todo")
+
+    userUrl.json()
+    for user in userUrl.json():
+        if user["id"] == args:
+            name = user["name"]
+
+    for todo in todoUrl.json():
+        if todo["userId"] == args and todo["completed"] == True:
+                complete += 1
+
+    for todo in todoUrl.json():
+        if todo["userId"] == args:
+            if todo["completed"] == True or todo["completed"] == False:
+                total += 1
+
+    print(f"Employee {name} is done with tasks({complete}/{total}):")
+    for todo in todoUrl.json():
+        if todo["userId"] == args and todo["completed"] == True:
+                print(f"\t{todo['title']}")
