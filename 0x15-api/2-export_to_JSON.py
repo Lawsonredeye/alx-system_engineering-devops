bc6cf4f3ed35b450a14ed5e0804fd3cf164b443f
lawsonredeye <omoregbeeolawson@gmail.com>
2024-04-23 02:49:59 +0100

    not completed yet

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+Python script that uses a fake API data to extract data for use
+"""
+
+import json
+import requests
+import sys
+
+
+def main():
+    """
+    main function which is the first poioint of execution for running this
+    script to get the userName, ID, title
+    """
+    # - Variables to be used
+    complete = 0
+    total = 0
+    username = ''
+    args = int(sys.argv[1])
+    userUrl = requests.get("https://jsonplaceholder.typicode.com/users")
+    todoUrl = requests.get("https://jsonplaceholder.typicode.com/todos")
+
+    userUrl.json()
+    for user in userUrl.json():
+        if user["id"] == args:
+            name = user["name"]
+            username = user["username"]
+
+    for todo in todoUrl.json():
+        if todo["userId"] == args:
+            if todo["completed"] is True:
+                complete += 1
+
+    for todo in todoUrl.json():
+        if todo["userId"] == args:
+            if todo["completed"] is True or todo["completed"] is False:
+                total += 1
+    
+    mydict = {}
+    for todo in todoUrl.json():
+        if todo["userId"] == args:
+            mydict["task"] = todo["title"]
+            mydict["completed"] = todo["completed"]
+            mydict["username"] = username
+    
+    filename = f"{args}.json"
+    with open(filename, "w") as f:
+        json.dump(mydict, f)
+
+
+if __name__ == "__main__":
+    main()
